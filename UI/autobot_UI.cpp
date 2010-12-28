d0c392fcc0af16ab7ce529dc860bd9f725c0893b
murusu <murusu0.0@gmail.com>
2010-12-28 16:46:35 +0800

    add base famre ui
    Signed-off-by: murusu <murusu0.0@gmail.com>

--- UI/autobot_UI.cpp
+++ UI/autobot_UI.cpp
@@ -16,21 +16,43 @@ AutoBotMainFrame::AutoBotMainFrame( wxWindow* parent, wxWindowID id, const wxStr
 	wxBoxSizer* bSizer12;
 	bSizer12 = new wxBoxSizer( wxVERTICAL );
 	
+	m_listCtrl_tasks = new wxListCtrl( this, wxID_ANY, wxPoint( -1,-1 ), wxSize( -1,300 ), wxLC_HRULES|wxLC_REPORT|wxLC_SINGLE_SEL|wxLC_VIRTUAL|wxLC_VRULES );
+	bSizer12->Add( m_listCtrl_tasks, 0, wxEXPAND|wxTOP, 2 );
+	
 	this->SetSizer( bSizer12 );
 	this->Layout();
 	m_statusBar1 = this->CreateStatusBar( 1, wxST_SIZEGRIP, wxID_ANY );
 	m_menubar1 = new wxMenuBar( 0 );
+	m_menu_system = new wxMenu();
+	wxMenuItem* m_menuItem_config;
+	m_menuItem_config = new wxMenuItem( m_menu_system, wxID_ANY, wxString( _("Config") ) , wxEmptyString, wxITEM_NORMAL );
+	m_menu_system->Append( m_menuItem_config );
+	
+	wxMenuItem* m_menuItem_exit;
+	m_menuItem_exit = new wxMenuItem( m_menu_system, wxID_ANY, wxString( _("Exit") ) , wxEmptyString, wxITEM_NORMAL );
+	m_menu_system->Append( m_menuItem_exit );
+	
+	m_menubar1->Append( m_menu_system, _("System") ); 
+	
 	m_menu_task = new wxMenu();
-	wxMenuItem* m_menuItem_addtask;
-	m_menuItem_addtask = new wxMenuItem( m_menu_task, wxID_ANY, wxString( _("Add a new task") ) , wxEmptyString, wxITEM_NORMAL );
-	m_menu_task->Append( m_menuItem_addtask );
+	wxMenuItem* m_menuItem_runtask;
+	m_menuItem_runtask = new wxMenuItem( m_menu_task, wxID_ANY, wxString( _("Run task") ) , wxEmptyString, wxITEM_NORMAL );
+	m_menu_task->Append( m_menuItem_runtask );
+	
+	wxMenuItem* m_menuItem_stoptask;
+	m_menuItem_stoptask = new wxMenuItem( m_menu_task, wxID_ANY, wxString( _("Stop task") ) , wxEmptyString, wxITEM_NORMAL );
+	m_menu_task->Append( m_menuItem_stoptask );
 	
 	wxMenuItem* m_separator1;
 	m_separator1 = m_menu_task->AppendSeparator();
 	
-	wxMenuItem* m_menuItem_exit;
-	m_menuItem_exit = new wxMenuItem( m_menu_task, wxID_ANY, wxString( _("Exit") ) , wxEmptyString, wxITEM_NORMAL );
-	m_menu_task->Append( m_menuItem_exit );
+	wxMenuItem* m_menuItem_addtask;
+	m_menuItem_addtask = new wxMenuItem( m_menu_task, wxID_ANY, wxString( _("Add task") ) , wxEmptyString, wxITEM_NORMAL );
+	m_menu_task->Append( m_menuItem_addtask );
+	
+	wxMenuItem* m_menuItem_deltask;
+	m_menuItem_deltask = new wxMenuItem( m_menu_task, wxID_ANY, wxString( _("Delete task") ) , wxEmptyString, wxITEM_NORMAL );
+	m_menu_task->Append( m_menuItem_deltask );
 	
 	m_menubar1->Append( m_menu_task, _("Task") ); 
 	
@@ -43,10 +65,221 @@ AutoBotMainFrame::AutoBotMainFrame( wxWindow* parent, wxWindowID id, const wxStr
 	
 	this->SetMenuBar( m_menubar1 );
 	
+	m_toolBar1 = this->CreateToolBar( wxTB_HORIZONTAL, wxID_ANY ); 
+	m_toolBar1->AddTool( wxID_ANY, _("tool"), wxNullBitmap, wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString ); 
+	m_toolBar1->AddTool( wxID_ANY, _("tool"), wxNullBitmap, wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString ); 
+	m_toolBar1->AddTool( wxID_ANY, _("tool"), wxNullBitmap, wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString ); 
+	m_toolBar1->AddSeparator(); 
+	m_toolBar1->AddTool( wxID_ANY, _("tool"), wxNullBitmap, wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString ); 
+	m_toolBar1->AddTool( wxID_ANY, _("tool"), wxNullBitmap, wxNullBitmap, wxITEM_NORMAL, wxEmptyString, wxEmptyString ); 
+	m_toolBar1->Realize();
+	
 	
 	this->Centre( wxBOTH );
 }
 
 AutoBotMainFrame::~AutoBotMainFrame()
+{
+}
+
+DialogTaskConfig::DialogTaskConfig( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	wxBoxSizer* bSizer2;
+	bSizer2 = new wxBoxSizer( wxVERTICAL );
+	
+	wxBoxSizer* bSizer3;
+	bSizer3 = new wxBoxSizer( wxVERTICAL );
+	
+	m_notebook_taskconfig = new wxNotebook( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+	m_panel_taskbase = new wxPanel( m_notebook_taskconfig, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	wxBoxSizer* bSizer8;
+	bSizer8 = new wxBoxSizer( wxVERTICAL );
+	
+	wxBoxSizer* bSizer15;
+	bSizer15 = new wxBoxSizer( wxVERTICAL );
+	
+	bSizer8->Add( bSizer15, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer9;
+	bSizer9 = new wxBoxSizer( wxHORIZONTAL );
+	
+	wxBoxSizer* bSizer11;
+	bSizer11 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_staticText1 = new wxStaticText( m_panel_taskbase, wxID_ANY, _("Task Name:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText1->Wrap( -1 );
+	bSizer11->Add( m_staticText1, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	m_textCtrl1 = new wxTextCtrl( m_panel_taskbase, wxID_ANY, wxEmptyString, wxDefaultPosition, wxSize( 150,-1 ), 0 );
+	bSizer11->Add( m_textCtrl1, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	bSizer9->Add( bSizer11, 1, wxEXPAND|wxLEFT, 5 );
+	
+	wxBoxSizer* bSizer13;
+	bSizer13 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_staticText2 = new wxStaticText( m_panel_taskbase, wxID_ANY, _("Action Type"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText2->Wrap( -1 );
+	bSizer13->Add( m_staticText2, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	wxString m_choice1Choices[] = { _("Interval"), _("Once"), _("Daily"), _("Weekly"), _("Monthly"), _("Yearly") };
+	int m_choice1NChoices = sizeof( m_choice1Choices ) / sizeof( wxString );
+	m_choice1 = new wxChoice( m_panel_taskbase, wxID_ANY, wxDefaultPosition, wxSize( 150,-1 ), m_choice1NChoices, m_choice1Choices, 0 );
+	m_choice1->SetSelection( 0 );
+	bSizer13->Add( m_choice1, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	bSizer9->Add( bSizer13, 1, wxEXPAND|wxLEFT, 5 );
+	
+	bSizer8->Add( bSizer9, 3, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer10;
+	bSizer10 = new wxBoxSizer( wxVERTICAL );
+	
+	m_panel_baseinterval = new wxPanel( m_panel_taskbase, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	wxBoxSizer* bSizer16;
+	bSizer16 = new wxBoxSizer( wxHORIZONTAL );
+	
+	wxBoxSizer* bSizer17;
+	bSizer17 = new wxBoxSizer( wxVERTICAL );
+	
+	m_staticText3 = new wxStaticText( m_panel_baseinterval, wxID_ANY, _("Interval:"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText3->Wrap( -1 );
+	bSizer17->Add( m_staticText3, 0, wxALL, 5 );
+	
+	bSizer16->Add( bSizer17, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer18;
+	bSizer18 = new wxBoxSizer( wxVERTICAL );
+	
+	wxBoxSizer* bSizer19;
+	bSizer19 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_textCtrl_basedays = new wxTextCtrl( m_panel_baseinterval, wxID_ANY, _("0"), wxDefaultPosition, wxSize( 50,-1 ), wxTE_RIGHT );
+	bSizer19->Add( m_textCtrl_basedays, 0, wxALIGN_CENTER_VERTICAL|wxRIGHT, 5 );
+	
+	m_staticText4 = new wxStaticText( m_panel_baseinterval, wxID_ANY, _("Days"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText4->Wrap( -1 );
+	bSizer19->Add( m_staticText4, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	bSizer18->Add( bSizer19, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer20;
+	bSizer20 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_textCtrl_basehours = new wxTextCtrl( m_panel_baseinterval, wxID_ANY, _("0"), wxDefaultPosition, wxSize( 50,-1 ), wxTE_RIGHT );
+	bSizer20->Add( m_textCtrl_basehours, 0, wxALIGN_CENTER_VERTICAL|wxRIGHT, 5 );
+	
+	m_staticText5 = new wxStaticText( m_panel_baseinterval, wxID_ANY, _("Hours"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText5->Wrap( -1 );
+	bSizer20->Add( m_staticText5, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	bSizer18->Add( bSizer20, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer21;
+	bSizer21 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_textCtrl_baseminutes = new wxTextCtrl( m_panel_baseinterval, wxID_ANY, _("0"), wxDefaultPosition, wxSize( 50,-1 ), wxTE_RIGHT );
+	bSizer21->Add( m_textCtrl_baseminutes, 0, wxALIGN_CENTER_VERTICAL|wxRIGHT, 5 );
+	
+	m_staticText6 = new wxStaticText( m_panel_baseinterval, wxID_ANY, _("Minutes"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText6->Wrap( -1 );
+	bSizer21->Add( m_staticText6, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	bSizer18->Add( bSizer21, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer22;
+	bSizer22 = new wxBoxSizer( wxHORIZONTAL );
+	
+	m_textCtrl_baseseconds = new wxTextCtrl( m_panel_baseinterval, wxID_ANY, _("0"), wxDefaultPosition, wxSize( 50,-1 ), wxTE_RIGHT );
+	bSizer22->Add( m_textCtrl_baseseconds, 0, wxALIGN_CENTER_VERTICAL|wxRIGHT, 5 );
+	
+	m_staticText7 = new wxStaticText( m_panel_baseinterval, wxID_ANY, _("Seconds"), wxDefaultPosition, wxDefaultSize, 0 );
+	m_staticText7->Wrap( -1 );
+	bSizer22->Add( m_staticText7, 0, wxALIGN_CENTER_VERTICAL|wxALL, 5 );
+	
+	bSizer18->Add( bSizer22, 1, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer23;
+	bSizer23 = new wxBoxSizer( wxVERTICAL );
+	
+	bSizer18->Add( bSizer23, 4, wxEXPAND, 5 );
+	
+	bSizer16->Add( bSizer18, 6, wxEXPAND, 5 );
+	
+	m_panel_baseinterval->SetSizer( bSizer16 );
+	m_panel_baseinterval->Layout();
+	bSizer16->Fit( m_panel_baseinterval );
+	bSizer10->Add( m_panel_baseinterval, 1, wxEXPAND | wxALL, 5 );
+	
+	bSizer8->Add( bSizer10, 21, wxEXPAND, 5 );
+	
+	m_panel_taskbase->SetSizer( bSizer8 );
+	m_panel_taskbase->Layout();
+	bSizer8->Fit( m_panel_taskbase );
+	m_notebook_taskconfig->AddPage( m_panel_taskbase, _("Base"), false );
+	m_panel_taskaction = new wxPanel( m_notebook_taskconfig, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_notebook_taskconfig->AddPage( m_panel_taskaction, _("Actions"), true );
+	m_panel_taskother = new wxPanel( m_notebook_taskconfig, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_notebook_taskconfig->AddPage( m_panel_taskother, _("Others"), false );
+	
+	bSizer3->Add( m_notebook_taskconfig, 1, wxEXPAND | wxALL, 5 );
+	
+	bSizer2->Add( bSizer3, 10, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer4;
+	bSizer4 = new wxBoxSizer( wxVERTICAL );
+	
+	m_button_tasksave = new wxButton( this, wxID_ANY, _("Save"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer4->Add( m_button_tasksave, 0, wxALIGN_RIGHT|wxALL, 5 );
+	
+	bSizer2->Add( bSizer4, 1, wxEXPAND, 5 );
+	
+	this->SetSizer( bSizer2 );
+	this->Layout();
+	
+	this->Centre( wxBOTH );
+}
+
+DialogTaskConfig::~DialogTaskConfig()
+{
+}
+
+DialogActionConfig::DialogActionConfig( wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style ) : wxDialog( parent, id, title, pos, size, style )
+{
+	this->SetSizeHints( wxDefaultSize, wxDefaultSize );
+	
+	wxBoxSizer* bSizer5;
+	bSizer5 = new wxBoxSizer( wxVERTICAL );
+	
+	wxBoxSizer* bSizer6;
+	bSizer6 = new wxBoxSizer( wxVERTICAL );
+	
+	m_notebook_actionconfig = new wxNotebook( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
+	m_panel_condition = new wxPanel( m_notebook_actionconfig, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_notebook_actionconfig->AddPage( m_panel_condition, _("Condition"), true );
+	m_panel_execution = new wxPanel( m_notebook_actionconfig, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
+	m_notebook_actionconfig->AddPage( m_panel_execution, _("Execution"), false );
+	
+	bSizer6->Add( m_notebook_actionconfig, 1, wxEXPAND | wxALL, 5 );
+	
+	bSizer5->Add( bSizer6, 10, wxEXPAND, 5 );
+	
+	wxBoxSizer* bSizer7;
+	bSizer7 = new wxBoxSizer( wxVERTICAL );
+	
+	m_button_actionsave = new wxButton( this, wxID_ANY, _("Save"), wxDefaultPosition, wxDefaultSize, 0 );
+	bSizer7->Add( m_button_actionsave, 0, wxALIGN_RIGHT|wxALL, 5 );
+	
+	bSizer5->Add( bSizer7, 1, wxEXPAND, 5 );
+	
+	this->SetSizer( bSizer5 );
+	this->Layout();
+	
+	this->Centre( wxBOTH );
+}
+
+DialogActionConfig::~DialogActionConfig()
 {
 }
--- UI/autobot_UI.h
+++ UI/autobot_UI.h
@@ -10,18 +10,27 @@
 
 #include <wx/intl.h>
 
-#include <wx/sizer.h>
+#include <wx/listctrl.h>
 #include <wx/gdicmn.h>
-#include <wx/statusbr.h>
 #include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
 #include <wx/string.h>
+#include <wx/sizer.h>
+#include <wx/statusbr.h>
 #include <wx/bitmap.h>
 #include <wx/image.h>
 #include <wx/icon.h>
 #include <wx/menu.h>
+#include <wx/toolbar.h>
 #include <wx/frame.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/choice.h>
+#include <wx/panel.h>
+#include <wx/notebook.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -34,15 +43,73 @@ class AutoBotMainFrame : public wxFrame
 	private:
 	
 	protected:
+		wxListCtrl* m_listCtrl_tasks;
 		wxStatusBar* m_statusBar1;
 		wxMenuBar* m_menubar1;
+		wxMenu* m_menu_system;
 		wxMenu* m_menu_task;
 		wxMenu* m_menu_help;
+		wxToolBar* m_toolBar1;
 	
 	public:
 		
 		AutoBotMainFrame( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Auto Bot"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 600,400 ), long style = wxCAPTION|wxCLOSE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL );
 		~AutoBotMainFrame();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class DialogTaskConfig
+///////////////////////////////////////////////////////////////////////////////
+class DialogTaskConfig : public wxDialog 
+{
+	private:
+	
+	protected:
+		wxNotebook* m_notebook_taskconfig;
+		wxPanel* m_panel_taskbase;
+		wxStaticText* m_staticText1;
+		wxTextCtrl* m_textCtrl1;
+		wxStaticText* m_staticText2;
+		wxChoice* m_choice1;
+		wxPanel* m_panel_baseinterval;
+		wxStaticText* m_staticText3;
+		wxTextCtrl* m_textCtrl_basedays;
+		wxStaticText* m_staticText4;
+		wxTextCtrl* m_textCtrl_basehours;
+		wxStaticText* m_staticText5;
+		wxTextCtrl* m_textCtrl_baseminutes;
+		wxStaticText* m_staticText6;
+		wxTextCtrl* m_textCtrl_baseseconds;
+		wxStaticText* m_staticText7;
+		wxPanel* m_panel_taskaction;
+		wxPanel* m_panel_taskother;
+		wxButton* m_button_tasksave;
+	
+	public:
+		
+		DialogTaskConfig( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Task Setting"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 600,400 ), long style = wxDEFAULT_DIALOG_STYLE );
+		~DialogTaskConfig();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class DialogActionConfig
+///////////////////////////////////////////////////////////////////////////////
+class DialogActionConfig : public wxDialog 
+{
+	private:
+	
+	protected:
+		wxNotebook* m_notebook_actionconfig;
+		wxPanel* m_panel_condition;
+		wxPanel* m_panel_execution;
+		wxButton* m_button_actionsave;
+	
+	public:
+		
+		DialogActionConfig( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _("Action Setting"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 600,400 ), long style = wxDEFAULT_DIALOG_STYLE );
+		~DialogActionConfig();
 	
 };
 
